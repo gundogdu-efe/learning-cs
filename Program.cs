848e6277d90723e74f66217980ad490f8d54ff84
Efe Gündoğdu <61597363+gundogdu-efe@users.noreply.github.com>
2022-02-04 17:41:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*namespace FirstPorject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            Console.ReadLine();
+        }
+    }
+}
+*/
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Efe";
+
+            int characterAge;
+            characterAge = 23;
+            Console.WriteLine("There once was a man named "+ characterName);
+            Console.WriteLine("He was "+ characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the named "+ characterName);
+            Console.WriteLine("But didn't like being "+ characterAge);
+        }
+    }
+}
